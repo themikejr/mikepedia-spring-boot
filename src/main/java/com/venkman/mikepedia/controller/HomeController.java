e17a1c5220ae4f3f2e2e928e51bce001e8f50d2e
Mike Brinker <themikejr@gmail.com>
2014-10-07 16:50:15 -0400

    Initial Commit.

---
+++ src/main/java/com/venkman/mikepedia/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.venkman.mikepedia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greeting(Model model) {
+        return "home";
+    }
+    
+    @RequestMapping("/blog")
+    public String blogEntries(Model model) {
+    	return "blog";
+    }
+
+}
